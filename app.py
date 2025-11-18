7a8836f83757aa23f0afe4130735e07de3278348
tejus2002 <tejus8964@gmail.com>
2025-11-18 20:06:25 +0530

    Fix return statement formatting in hello function

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello! My DevOps Project is Live."Success! I updated this without touching a terminal!""
+    return "Hello! My DevOps Project is Live.Success! I updated this without touching a terminal!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
